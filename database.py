3375adc603973198956a206dc89731e6f038f7f4
aniketjagdale02 <aniket.jagdale02@gmail.com>
2025-07-23 09:48:57 +0530

    Add files via upload

--- database.py
+++ database.py
@@ -1,12 +1,19 @@
-from sqlalchemy import create_engine
+
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-# Read DATABASE_URL from environment variable
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "sqlite:///./tickets.db"
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
+
+class Ticket(Base):
+    __tablename__ = "tickets"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
